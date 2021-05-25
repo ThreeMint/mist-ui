fdcddebff8e3519ac69caf79d20894757e8de5fe
ThreeMint <mistsripples@163.com>
2021-05-25 23:50:38 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+//样式
+import '../modules/mist-ui/theme-chalk/index.scss'
+
+// 导入方式1
+import MistUI from '../modules/mist-ui'
+Vue.use(MistUI);
+// Vue.use(MistUI, {
+//   components: [
+//     'MistButton'
+//   ]
+// })
+
+// 导入方式2
+// element等ui采用的导入方式
+// import {MistButton} from '../modules/mist-ui'
+// Vue.use(MistButton)
 
 Vue.config.productionTip = false
 
