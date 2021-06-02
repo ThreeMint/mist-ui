217926cf32d69de2b7d3c3ac1aa7810dd0ab71ea
ThreeMint <mistsripples@163.com>
2021-06-02 22:42:57 +0800

    add btn

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>button按钮</h2>
+    <div class="button">
+      <mist-button type="default">默认按钮</mist-button>
+      <br />
+      <mist-button type="success" round>圆形按钮</mist-button>
+      <br />
+      <mist-button type="success" circle>圆</mist-button>
+      <br />
+      <mist-button type="primary" disabled>主要按钮</mist-button>
+      <br />
+      <mist-button type="success" round disabled>圆形按钮</mist-button>
+      <br />
+      <mist-button type="primary" round size="small">小型按钮</mist-button>
+      <br />
+      <mist-button type="default" circle size="medium">中</mist-button>
+      <br />
+      <mist-button :loading="true">加载中</mist-button>
+    </div>
+    <!-- <hr /> -->
+    <h2>link链接</h2>
+    <div class="link">
+      <h3>默认</h3>
+      <mist-link>有下划线</mist-link>
+      <mist-link type="success" :underline="false">无下划线</mist-link>
+      <h3>disabled</h3>
+      <mist-link disabled>有下划线</mist-link>
+      <mist-link type="primary" disabled>谢谢谢谢</mist-link>
+    </div>
+
+    <h2>Switch链接</h2>
+    <div class="switch">
+      <mist-switch
+        disabled
+        :value.sync="outValue"
+        active-color="#ff0000"
+        inactive-color="#000000"
+        @change="switchClick"
+      ></mist-switch>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      //button
+      load: false,
+      //switch
+      outValue: true,
+    };
+  },
+  methods: {
+    switchClick(val) {
+      console.log(val);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .button {
+    button {
+      margin-left: 5px;
+      margin-bottom: 5px;
+    }
+  }
+  .button,
+  .link,
+  .switch {
+    border: 1px solid #999;
+    overflow: scroll;
+    height: 300px;
+  }
 }
 </style>
